5e4384f9c2cb4e1dc51b7964c98299190b6abec1
jc495193 <louis.bortolotto@my.jcu.edu.au>
2019-09-12 12:04:05 +1000

    finish Guitars!

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,9 @@
+from prac_06.guitar import Guitar
+
+gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+another = Guitar("Another Guitar", 2012, 209)
+
+print(Guitar.get_age(gibson))
+print(Guitar.get_age(another))
+print(Guitar.is_vintage(gibson))
+print(Guitar.is_vintage(another))
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,21 +1,14 @@
 class ProgrammingLanguage:
 
     def __init__(self, field="", typing="", reflection=True, year=0):
-        """Initialise a Car instance.
 
-        fuel: float, one unit of fuel drives one kilometre
-        """
         self.field = field
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
-        """Add amount to the car's fuel."""
-        if self.typing.upper() == "DYNAMIC":
-            return True
-        else:
-            return False
+        return self.typing.upper() == "DYNAMIC"
 
     def __str__(self):
         return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.field, self.typing, self.reflection,
