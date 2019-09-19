0c380658c5a1d5a9a31b4da0754d07febfd7fae6
louisbortolotto <louisbortolotto@gmail.com>
2019-09-19 10:20:21 +1000

    finish guitar test

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -3,10 +3,10 @@ from prac_06.guitar import Guitar
 gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
 another = Guitar("Another Guitar", 2012, 209)
 
-print(Guitar.get_age(gibson))
-print(gibson.get_age())
-print(Guitar.get_age(another))
-print(Guitar.is_vintage(gibson))
-print(Guitar.is_vintage(another))
+print("Guitar.get_age(gibson) expected 97, got", Guitar.get_age(gibson))
+print("gibson.get_age expected 97, got", gibson.get_age())
+print("Guitar.get_age(another) expected 7, got", Guitar.get_age(another))
+print("Guitar.is_vintage(gibson) expected True, got", Guitar.is_vintage(gibson))
+print("Guitar.is_vintage(another) expected False, got", Guitar.is_vintage(another))
 
 print(Guitar.get_age(gibson))
